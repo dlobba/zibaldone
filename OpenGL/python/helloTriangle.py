34b8e01d7e32bfc49956cb6a0e7cb362eab5e497
dlobba <d.lobba@campus.unimib.it>
2017-04-17 21:16:56 +0200

    Fixed issue betweeen translation from c to python

--- OpenGL/python/helloTriangle.py
+++ OpenGL/python/helloTriangle.py
@@ -1,21 +1,25 @@
-import glfw
 import sys
 from OpenGL.GLUT import *
 from OpenGL.GL import *
+import glfw
+
+import numpy as np
+
 
 shaderSource = """
+    #version 330
     layout (location = 0) in vec3 position;
     void main ()
     {
         gl_Position = vec4 (position.x, position.y, position.z, 1.0);
     }"""
 
-fragmentSource = \
-    "out vec4 color;" \
-    "void main ()" \
-    "{" \
-        "color = vec4 (1.0f, 0.5f, 0.2f, 1.0f);" \
-    "}";
+fragmentSource = """
+    out vec4 color;
+    void main ()
+    {
+        color = vec4 (1.0f, 0.5f, 0.2f, 1.0f);
+    }"""
 
 def main():
     # Initialize the library
@@ -38,9 +42,9 @@ def main():
     success = 0
     infoLog = ""
 
-    glGetShaderiv (vertexShader, GL_COMPILE_STATUS, success)
+    success = glGetShaderiv (vertexShader, GL_COMPILE_STATUS)
 
-    if not success:
+    if success != GL_TRUE:
         print ("Vertex shader compilation failed: " + str(glGetShaderInfoLog (vertexShader)))
 
     fragmentShader = glCreateShader (GL_FRAGMENT_SHADER)
@@ -49,9 +53,9 @@ def main():
 
     success = 0
     infoLog = ""
-    glGetShaderiv (fragmentShader, GL_COMPILE_STATUS, success)
+    success = glGetShaderiv (fragmentShader, GL_COMPILE_STATUS)
     if not success:
-        print ("Fragment shader compilation failed: ")
+        print ("Fragment shader compilation failed: " + str(glGetShaderInfoLog (fragmentShader)))
 
     shaderProgram = glCreateProgram ()
     glAttachShader (shaderProgram, vertexShader)
@@ -59,28 +63,32 @@ def main():
     glLinkProgram (shaderProgram)
 
     # test for linkage
-    glGetProgramiv (shaderProgram, GL_LINK_STATUS, success)
+    success = glGetProgramiv (shaderProgram, GL_LINK_STATUS)
     if not success:
-        print ("Failed to link the shaders to the shader program: ")
+        print ("Failed to link the shaders to the shader program: " + str(glGetProgramInfoLog (shaderProgram)))
 
     glUseProgram (shaderProgram)
     glDeleteShader (vertexShader)
     glDeleteShader (fragmentShader)
 
-    vertices = (
-        -0.5, -0.5, 0.0,
-        0.5, -0.5, 0.0,
-        0.0, 0.5, 0.0
+    vertices = np.array(
+        [-0.5, -0.5, 0.0,
+         0.5, -0.5, 0.0,
+         0.0, 0.5, 0.0],
+        dtype = 'float32'
         )
+    vertexDimension = 3
+    nVertexed = 3
+    
     VBO = VAO = EBO = None
 
-    glGenVertexArrays (1, VAO)
-    glGenBuffer (1, VBO)
+    VAO = glGenVertexArrays (1)
+    VBO = glGenBuffers (1)
 
     glBindVertexArray (VAO)
     glBindBuffer (GL_ARRAY_BUFFER, VBO)
-    glBufferData (GL_ARRAY_BUFFER, sys.getsizeof(vertices), vertices, GL_STATIC_DRAW)
-    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 3, 0)
+    glBufferData (GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW)
+    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, None)
     glEnableVertexAttribArray (0)
 
     glBindBuffer (GL_ARRAY_BUFFER, 0)
@@ -88,21 +96,23 @@ def main():
     
     # Loop until the user closes the window
     while not glfw.window_should_close(window):
+
+        # Poll for and process events
+        glfw.poll_events()
+
+
         # Render here, e.g. using pyOpenGL
 
         glClearColor (0.2, 0.3, 0.3, 1.0)
         glClear (GL_COLOR_BUFFER_BIT)
-
+    
         glUseProgram (shaderProgram)
         glBindVertexArray (VAO)
-        glDrawArray (GL_TRIANGLE, 0, 3)
+        glDrawArrays (GL_TRIANGLES, 0, 3)
         glBindVertexArray (0)
         
         # Swap front and back buffers
         glfw.swap_buffers(window)
-
-        # Poll for and process events
-        glfw.poll_events()
 
     glfw.terminate()
 
